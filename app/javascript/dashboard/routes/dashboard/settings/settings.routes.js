0252ea194bdfe23c5ccdeca671f8c353605515e3
JaeanHan <jaean1999@naver.com>
2025-05-07 19:18:34 +0900

    feat: 설정 탭 감사 로그, 맞춤 역할, SLA 네비게이션, 라우터에서 주석

--- app/javascript/dashboard/routes/dashboard/settings/settings.routes.js
+++ app/javascript/dashboard/routes/dashboard/settings/settings.routes.js
@@ -7,20 +7,20 @@ import {
 import account from './account/account.routes';
 import agent from './agents/agent.routes';
 import agentBot from './agentBots/agentBot.routes';
-import attributes from './attributes/attributes.routes';
+// import attributes from './attributes/attributes.routes';
 import automation from './automation/automation.routes';
-import auditlogs from './auditlogs/audit.routes';
+// import auditlogs from './auditlogs/audit.routes';
 import billing from './billing/billing.routes';
 import canned from './canned/canned.routes';
 import inbox from './inbox/inbox.routes';
-import integrations from './integrations/integrations.routes';
+// import integrations from './integrations/integrations.routes';
 import labels from './labels/labels.routes';
 import macros from './macros/macros.routes';
 import reports from './reports/reports.routes';
 import store from '../../../store';
-import sla from './sla/sla.routes';
-import teams from './teams/teams.routes';
-import customRoles from './customRoles/customRole.routes';
+// import sla from './sla/sla.routes';
+// import teams from './teams/teams.routes';
+// import customRoles from './customRoles/customRole.routes';
 import profile from './profile/profile.routes';
 
 export default {
@@ -45,19 +45,19 @@ export default {
     ...account.routes,
     ...agent.routes,
     ...agentBot.routes,
-    ...attributes.routes,
+    // ...attributes.routes,
     ...automation.routes,
-    ...auditlogs.routes,
+    // ...auditlogs.routes,
     ...billing.routes,
     ...canned.routes,
     ...inbox.routes,
-    ...integrations.routes,
+    // ...integrations.routes,
     ...labels.routes,
     ...macros.routes,
     ...reports.routes,
-    ...sla.routes,
-    ...teams.routes,
-    ...customRoles.routes,
+    // ...sla.routes,
+    // ...teams.routes,
+    // ...customRoles.routes,
     ...profile.routes,
   ],
 };
